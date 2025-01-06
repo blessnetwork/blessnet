a33cdd62997fc02c248d8ba7e29fc90f1de2795d
Derek Anderson <dmikey@users.noreply.github.com>
2025-01-05 21:29:57 -0600

    create archive
    Signed-off-by: Derek Anderson <dmikey@users.noreply.github.com>

--- commands/deploy.js
+++ commands/deploy.js
@@ -1,9 +1,57 @@
 const { Command } = require('commander');
+const { createWasmManifest } = require('../lib/manifest');
+const { createWasmArchive } = require('../lib/archive');
+const fs = require('node:fs');
+const path = require('node:path');
+const { execSync } = require('node:child_process');
+const crypto = require('crypto');
 
 const deployCommand = new Command('deploy')
     .description('Deploy your project')
     .action(() => {
         console.log('Deploy command executed');
+
+        // Check if release build exists
+        const releasePath = path.join(__dirname, '../foo/build/release.wasm');
+        if (!fs.existsSync(releasePath)) {
+            console.log('Release build not found. Building release.wasm...');
+            execSync('npm run build:release', { stdio: 'inherit' });
+        }
+
+        // Rename release.wasm to the package.json name
+        const packageJson = require('../package.json');
+        const wasmName = `${packageJson.name}.wasm`;
+        const renamedWasmPath = path.join(__dirname, `../foo/build/${wasmName}`);
+        fs.renameSync(releasePath, renamedWasmPath);
+        console.log(`Renamed release.wasm to ${wasmName}`);
+
+        // Generate manifest.json
+        const manifest = createWasmManifest(wasmName, 'application/javascript');
+        const manifestPath = path.join(__dirname, '../foo/build/manifest.json');
+        fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+        console.log(`Manifest generated at ${manifestPath}`);
+
+        // Create archive of manifest.json and the renamed wasm file
+        const archiveName = `${packageJson.name}.tar.gz`;
+        const archivePath = path.join(__dirname, `../foo/build/${archiveName}`);
+        createWasmArchive(path.join(__dirname, '../foo/build'), archiveName, wasmName);
+        console.log(`Archive created at ${archivePath}`);
+
+        // Update manifest with new values
+        const wasmMd5 = crypto.createHash('md5').update(fs.readFileSync(renamedWasmPath)).digest('hex');
+        const archiveChecksum = crypto.createHash('sha256').update(fs.readFileSync(archivePath)).digest('hex');
+        manifest.modules = [{
+            file: wasmName,
+            name: packageJson.name,
+            type: 'entry',
+            md5: wasmMd5
+        }];
+        manifest.runtime = {
+            url: archiveName,
+            checksum: archiveChecksum
+        };
+        fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+        console.log(`Manifest updated at ${manifestPath}`);
     });
 
 module.exports = deployCommand;
