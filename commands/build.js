b4dde8827a6093929cd8d97e32fe1ae97757abf0
Derek Anderson <dmikey@users.noreply.github.com>
2025-01-05 20:22:32 -0600

    pass through execute
    Signed-off-by: Derek Anderson <dmikey@users.noreply.github.com>

--- commands/build.js
+++ commands/build.js
@@ -1,11 +1,12 @@
-
 const { Command } = require('commander');
 const { execSync } = require('node:child_process');
 
 const buildCommand = new Command('build')
     .description('Build the project')
-    .action(() => {
-        execSync('npm run build:release', { stdio: 'inherit' });
+    .option('--debug', 'Build in debug mode') // Add debug option
+    .action((options) => {
+        const buildCommand = options.debug ? 'npm run build:debug' : 'npm run build:release';
+        execSync(buildCommand, { stdio: 'inherit' });
     });
 
 module.exports = buildCommand;
--- commands/preview.js
+++ commands/preview.js
@@ -1,6 +1,6 @@
 const { Command } = require('commander');
 const { startServer } = require('../lib/server');
-const { execSync } = require('node:child_process');
+const { run } = require('../lib/invoke'); // Corrected to use run function from invoke.js
 
 const previewCommand = new Command('preview')
     .description('Preview your project');
@@ -13,9 +13,13 @@ previewCommand
     });
 
 previewCommand
-    .action(() => {
-        const result = execSync('echo "hello"').toString();
-        console.log(result);
+    .action(async () => { // Updated to use async/await
+        try {
+            const result = await run({ stdin: 'echo "hello"' }); // Pass options to run function
+            console.log(result);
+        } catch (error) {
+            console.error('Error:', error);
+        }
     });
 
 module.exports = previewCommand;
