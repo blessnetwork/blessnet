dae5c48f542b76f7cf7908aad5b3f5f99623d0f3
Derek Anderson <dmikey@users.noreply.github.com>
2025-01-14 12:00:25 -0600

    update readme and manage
    Signed-off-by: Derek Anderson <dmikey@users.noreply.github.com>

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Build distributed, scalable, edge based applications fast.
 
 ## getting started
 
-Make sure you have `npm` installed. We recommend a `version 18+` the fastest way to getting started with npm is installing it into your system using `nvm`. 
+Make sure you have `npm` installed. We recommend a `version 18+` the fastest way to getting started with npm is installing it into your system using `nvm`.
 
-* https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
+* <https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating>
 
-Where ever you are, run `npx blessnet` to get context feedback. If calling from inside a project folder, information about the state of the project will be returned. 
+Where ever you are, run `npx blessnet` to get context feedback. If calling from inside a project folder, information about the state of the project will be returned.
 
 Otherwise the guided initializer will be called.
 
@@ -40,6 +40,7 @@ cd foo
 ```
 
 ### project structure
+
 ```text
 .
 ├── bls.toml              # project config
@@ -49,6 +50,18 @@ cd foo
 ├── tsconfig.debug.json   # typescript configs
 └── tsconfig.release.json # typescript configs
 ```
+
+### deploy
+
+To deploy the application, cd into the directory and run
+
+```bash
+npx blessnet deploy
+```
+
+### deploy keys
+
+**WARN**
 
 ## other things
 
--- commands/manage.js
+++ commands/manage.js
@@ -1,9 +1,35 @@
 const { Command } = require('commander');
+const { parseBlsConfig, saveBlsConfig } = require('../lib/config');
+const readlineSync = require('readline-sync');
+const chalk = require('chalk');
+const path = require('path');
 
 const manageCommand = new Command('manage')
     .description('Manage your project')
     .action(() => {
-        console.log('Manage command coming soon!');
+        const cwd = process.cwd();
+        const config = parseBlsConfig(cwd);
+        console.log(`Current Return Type is ${config.type}`);
+
+        const answer = readlineSync.question(chalk.yellow('Would you like to change anything about the project? (yes/no): '));
+        if (['yes', 'y'].includes(answer.toLowerCase())) {
+            const changeType = readlineSync.question(chalk.yellow('Would you like to change the project return type? (yes/no): '));
+            if (['yes', 'y'].includes(changeType.toLowerCase())) {
+                const types = ['text', 'json', 'html', 'raw'];
+                const index = readlineSync.keyInSelect(types, chalk.yellow('Select the new project type:'));
+                if (index !== -1) {
+                    config.type = types[index];
+                    saveBlsConfig(config, cwd);
+                    console.log(chalk.green('Project type updated successfully.'));
+                } else {
+                    console.log('No changes will be made to the return type.\n');
+                }
+            } else {
+                console.log('No changes will be made to the project type.');
+            }
+        } else {
+            console.log('No changes will be made.');
+        }
     });
 
 module.exports = manageCommand;
