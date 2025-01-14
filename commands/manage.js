40c2dea152f5753eeedb18bfef0c562b0f58c9d1
Derek Anderson <dmikey@users.noreply.github.com>
2025-01-14 14:12:13 -0600

    update manage
    Signed-off-by: Derek Anderson <dmikey@users.noreply.github.com>

--- commands/manage.js
+++ commands/manage.js
@@ -2,10 +2,36 @@ const { Command } = require('commander');
 const { parseBlsConfig, saveBlsConfig } = require('../lib/config');
 const readlineSync = require('readline-sync');
 const chalk = require('chalk');
-const path = require('path');
+const path = require('node:path');
 
 const manageCommand = new Command('manage')
-    .description('Manage your project')
+    .description('Manage your project');
+
+manageCommand
+    .command('type [newType]')
+    .description('Change the project return type')
+    .action((newType) => {
+        const cwd = process.cwd();
+        const config = parseBlsConfig(cwd);
+        const types = ['text', 'json', 'html', 'raw'];
+        let selectedType;
+
+        if (!newType || !types.includes(newType)) {
+            const index = readlineSync.keyInSelect(types, chalk.yellow('Select the new project type:'));
+            if (index !== -1) {
+                selectedType = types[index];
+            } else {
+                console.log('No changes will be made to the return type.\n');
+                return;
+            }
+        }
+
+        config.type = newType || selectedType;
+        saveBlsConfig(config, cwd);
+        console.log(chalk.green('Project type updated successfully.'));
+    });
+
+manageCommand
     .action(() => {
         const cwd = process.cwd();
         const config = parseBlsConfig(cwd);
