be8d077c856d1580fc96786fb71e8d32f8507481
Derek Anderson <dmikey@users.noreply.github.com>
2025-02-12 14:36:43 -0600

    bundle static assets with code
    Signed-off-by: Derek Anderson <dmikey@users.noreply.github.com>

--- commands/build.js
+++ commands/build.js
@@ -1,10 +1,16 @@
 const { Command } = require('commander');
 const { execSync } = require('node:child_process');
+const { scanJSFiles, createAssetsJson } = require('../lib/find-static-paths');
 
 const buildCommand = new Command('build')
     .description('Build the project')
-    .option('--debug', 'Build in debug mode') // Add debug option
+    .option('--debug', 'Build in debug mode')
     .action((options) => {
+        console.log('Scanning for static path configurations...');
+        const staticPaths = scanJSFiles(process.cwd());
+        createAssetsJson(staticPaths);
+        console.log('Generated assets file: bls.assets.json');
+
         const buildCommand = options.debug ? 'npm run build:debug' : 'npm run build:release';
         execSync(buildCommand, { stdio: 'inherit' });
     });
