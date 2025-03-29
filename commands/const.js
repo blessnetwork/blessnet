cacc5f1193b3a400bf2a1d1952d741ad6f32de58
Joinhack <joinhack@gmail.com>
2025-03-29 17:53:45 +0800

    for the stake result view

--- commands/const.js
+++ commands/const.js
@@ -5,7 +5,23 @@ const BLESNET_HOME = '.blessnet'
 
 const BLESSNET_DIR = path.join(os.homedir(), BLESNET_HOME)
 
+const SOLANA_CLUSTERS = [{
+    name: 'mainnet',
+    url: 'https://api.mainnet-beta.solana.com'
+}, {
+    name: 'testnet',
+    url: 'https://api.testnet.solana.com'
+}, {
+    name: 'devnet',
+    url: 'https://api.devnet.solana.com'
+}, {
+    name: 'localnet',
+    url: 'http://localhost:8899'
+}]
+
 module.exports = {
     BLESNET_HOME,
-    BLESSNET_DIR
+    BLESSNET_DIR,
+    SOLANA_CLUSTERS
 }
+
--- commands/registry.js
+++ commands/registry.js
@@ -3,11 +3,12 @@ const {
     Argument 
 } = require('commander')
 const anchor = require('@coral-xyz/anchor')
-const { BLESSNET_DIR } = require('./const')
+const { BLESSNET_DIR, SOLANA_CLUSTERS } = require('./const')
 const path = require('node:path')
 const fs = require('node:fs')
 const process = require('node:process')
 const chalk = require('chalk')
+const {LAMPORTS_PER_SOL} =  require('@solana/web3.js')
 const readline = require('node:readline')
 const readWallet = require('./walletUtils')
 
@@ -18,6 +19,16 @@ const registryCommand = new Command('registry')
     .description('registry: registry action bind the bless node with wallet')
 const walletArg = new Argument('wallet', 'wallet name')
 walletArg.required = true
+
+const getProvider = (cluster) => {
+    let url = null;
+    SOLANA_CLUSTERS.forEach((item) => {
+        if (item.name === cluster) {
+            url = item.url;
+        }
+    })
+    return url||cluster
+}
 
 registryCommand
     .addArgument(walletArg)
@@ -42,7 +53,8 @@ registryCommand
             const keypair = Keypair.generate()
             fs.writeFileSync(idFile, JSON.stringify(Array.from(keypair.secretKey)))
         }
-        process.env['ANCHOR_PROVIDER_URL'] = "https://api.devnet.solana.com"
+        const endpoint = getProvider(options.cluster)
+        process.env['ANCHOR_PROVIDER_URL'] = endpoint
         process.env['ANCHOR_WALLET'] = idFile
         const client = new blsClient.BlsRegisterClient()
         const rl = readline.createInterface({
@@ -50,7 +62,32 @@ registryCommand
             output: process.stdout,
             terminal: true
         });
-        rl.question('Enter the encryption key: ', (encryptionKey) => {
+
+        const stake = () => {
+            const stakeRl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout,
+                terminal: true
+            });
+            stakeRl.question('Enter the stake amount: ', async (amount) => {
+                stakeRl.close()
+                let amountNum = parseInt(amount)
+                if (isNaN(amountNum)) {
+                    console.log(chalk.red('The stake amount must be a number.'))
+                    process.exit(1)
+                }
+                if (amountNum < 1) {
+                    console.log(chalk.red('The stake amount must be great than 1.'))
+                    process.exit(1)
+                }
+                const amBN = new anchor.BN(amount).mul(new anchor.BN(LAMPORTS_PER_SOL))
+                const result = await client.registerClient.stake(amBN)
+                console.log(chalk.green(`The stake transaction is: https://explorer.solana.com/transaction/${result}?cluster=custom&customUrl=${endpoint}`))
+                
+            })
+        }
+
+        rl.question('Enter the encryption key: ',async (encryptionKey) => {
             rl.close()
             if (!encryptionKey) {
                 console.log(chalk.red('An encryption key is required.'));
@@ -58,6 +95,15 @@ registryCommand
             }
             const walletKeypair = readWallet(walletFile, encryptionKey)
             client.setWallet(new anchor.Wallet(walletKeypair))
+            const balance = await client.getBalance(walletKeypair.publicKey)
+            const balanceBN = new anchor.BN(balance).div(new anchor.BN(LAMPORTS_PER_SOL));
+            console.log(chalk.green(`The wallet "${walletKeypair.publicKey}" balance is: ${balanceBN.toNumber()}`))
+            if (balance <= 0) {
+                console.log(chalk.red('The wallet balance is 0, please fund it first.'))
+                process.exit(1)
+            }
+            stake()
+            
         })
     })
 
